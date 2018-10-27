0b7a96c6dc5b63192ae512717179370d8b98e375
Christopher Boyle <chrisboyle1993@gmail.com>
2018-10-27 19:51:28 -0400

    layout and icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={'./cslogotp.PNG'} className="App-logo"></img>
+          </header>
+        </div>
+        <div align='center' style={{ backgroundColor:'White'}}>
+        <h1 style={{color:'dodgerblue'}}>Hello from Codespout!</h1>
+          <div class="row">
+           <div class="column" style={{background:'#282c34'}}>
+           <br/>
+           <img src={'./csapp.PNG'} className="App-logo2"></img>
+           <h1 style={{color:'white'}}>App Development</h1>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           </div>
+            <div class="column" style={{background:'#282c34'}}>
+            <br/>
+            <img src={'./compcs.PNG'} className="App-logo2"></img>
+            <h1 style={{color:'white'}}>Web Design</h1>
+            <p fontSize='40' style={{color:'white'}}>Explore endless possibilities of growing your business and improving your center with the CodeSpout web development team.</p>
+            <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+            </div>
+            <div class="column" style={{background:'#282c34'}}>
+            <br/>
+            <img src={'./csgraphics.PNG'} className="App-logo2"></img>
+            <h1 style={{color:'white'}}>Graphic Design</h1>
+            <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+           <br/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
